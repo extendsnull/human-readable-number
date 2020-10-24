b594cdff12ba67b31c60dbab59fb5ff065563e95
Nick Petrenko <extendsnull@gmail.com>
2020-10-24 12:09:22 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
+const arr = x => Array.from(x);
+const num = x => Number(x) || 0;
+const str = x => String(x);
+const isEmpty = xs => xs.length === 0;
+const take = n => xs => xs.slice(0,n);
+const drop = n => xs => xs.slice(n);
+const reverse = xs => xs.slice(0).reverse();
+const comp = f => g => x => f (g (x));
+const not = x => !x;
+const chunk = n => xs =>
+    isEmpty(xs) ? [] : [take(n)(xs), ...chunk (n) (drop (n) (xs))];
+
+const a = [
+    '', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+
+const b = [
+    '', '', 'twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+];
+
+const g = [
+    '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
+];
+
 module.exports = function toReadable (number) {
-  
+    let makeGroup = ([ones, tens, huns]) => {
+        return [
+            num(huns) === 0 ? '' : a[huns] + ' hundred ',
+            num(ones) === 0 ? b[tens] : b[tens] && b[tens] + ' ' || '',
+            a[tens + ones] || a[ones]
+        ].join('');
+    };
+
+    let thousand = (group, i) => group === '' ? group : `${group} ${g[i]}`;
+
+    if (typeof number === 'number') {
+        return toReadable(String(number));
+    } if (number === '0') {
+        return 'zero';
+    } else {
+        return comp (chunk(3)) (reverse) (arr(number))
+            .map(makeGroup)
+            .map(thousand)
+            .filter(comp(not)(isEmpty))
+            .reverse()
+            .join('')
+            .trim();
+    }
 }
